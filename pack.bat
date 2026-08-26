0efd35f89e9ab7a063b3e8c3bf7073bfe6479536
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-08-26 08:30:45 +0800

    fix: remove PDB files from package output

--- pack.bat
+++ pack.bat
@@ -28,6 +28,8 @@ if errorlevel 1 goto :error
 dotnet pack "src\CodeWF.AvaloniaControls.TreeDataGrid.Themes\CodeWF.AvaloniaControls.TreeDataGrid.Themes.csproj" -c %CONFIGURATION% --no-build -o "%PACKAGES_DIR%"
 if errorlevel 1 goto :error
 
+for /r "%PACKAGES_DIR%" %%F in (*.pdb) do del /q "%%F" 2>nul
+
 echo.
 echo Packages are available in:
 echo %PACKAGES_DIR%
